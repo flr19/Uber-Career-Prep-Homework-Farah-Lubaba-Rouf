6dd77de29fb6d53f70a1b0d90c8be3bacbad32c4
Farah Lubaba Rouf <farah.adrita@gmail.com>
2021-04-17 04:13:41 +0300

    stack

---
+++ Assignment-1/Part 3/src/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        Stack mystack=new Stack();
+        mystack.push(1);
+        mystack.push(2);
+        mystack.push(3);
+        System.out.println(mystack.top());
+        System.out.println(mystack.pop());
+        System.out.println(mystack.pop());
+        System.out.println(mystack.pop());
+
+	// write your code here
+    }
+}
